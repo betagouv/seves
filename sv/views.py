d79708dd7caf0b1de122922c50848c3075931355
alan <alanzirek@gmail.com>
2024-06-11 14:52:55 +0200

    Erreur import (ajout TemplateView)

--- sv/views.py
+++ sv/views.py
@@ -7,6 +7,7 @@ from django.views.generic import (
     DetailView,
     CreateView,
     UpdateView,
+    TemplateView,
 )
 from django.urls import reverse
 from django.db.models import OuterRef, Subquery, Prefetch
