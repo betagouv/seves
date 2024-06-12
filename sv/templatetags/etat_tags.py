f175a466f6782e0725d55055cf5344d7f7282c1d
alan <alanzirek@gmail.com>
2024-06-12 16:39:48 +0200

    Création d'un filtre de template personnalisé pour gerer la couleur de fond du badge de l'état de la fiche détection.

---
+++ sv/templatetags/etat_tags.py
@@ -0,0 +1,18 @@
+from django import template
+
+register = template.Library()
+
+# Selection des classes CSS du composant badge du DSFR pour les états d'une fiche détection
+# https://www.systeme-de-design.gouv.fr/elements-d-interface/composants/badge
+ETATS_FICHE_DETECTION_COLORS = {
+    "nouveau": "success",  # Vert
+    "en cours": "info",  # Bleu
+    "fin de suivi": "new",  # Orange
+    "clôturé": "error",  # Rouge
+}
+
+
+@register.filter(name="etat_fiche_detection_color")
+def etat_fiche_detection_color(value):
+    print(value)
+    return ETATS_FICHE_DETECTION_COLORS.get(value)
