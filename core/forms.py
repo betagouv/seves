28f3912f3cc03edcc8f52eb97108f50e24f472d5
Anto59290 <antonin59290@hotmail.com>
2024-06-26 15:36:22 +0200

    More design

--- core/forms.py
+++ core/forms.py
@@ -2,11 +2,37 @@ from django.contrib.contenttypes.models import ContentType
 
 from core.models import Document
 from django import forms
+from collections import defaultdict
 
-class DocumentUploadForm(forms.ModelForm):
+class DSFRForm(forms.BaseForm):
+    default_input_class = "fr-input"
+
+    input_to_class=defaultdict(lambda: "fr-input")
+    input_to_class["ClearableFileInput"] = "fr-upload"
+    input_to_class["Select"] = "fr-select"
+
+    def as_dsfr_div(self):
+        return self.render("core/_dsfr_div.html")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields:
+            widget = self.fields[field].widget
+            class_to_add = self.input_to_class[type(widget).__name__]
+            widget.attrs["class"] = widget.attrs.get("class", "") + class_to_add
+
+
+
+class DocumentUploadForm(DSFRForm, forms.ModelForm):
+    nom = forms.CharField(help_text="Nommer le document de manière claire et compréhensible pour tous", label="Intitulé du document")
+    document_type = forms.ChoiceField(choices=Document.DOCUMENT_TYPE_CHOICES, label="Type de document")
+    description = forms.CharField(widget=forms.Textarea(attrs={"cols": 30, "rows": 4}), label="Description - facultatif", required=False)
+    file = forms.FileField(label="Ajouter un Document", help_text="Lorem ipsum dolor sit amet, consectetur adipiscing")
+
     class Meta:
         model = Document
-        fields = ['nom', 'file', 'content_type', 'object_id']
+        fields = ['nom', 'document_type', 'description', 'file', 'content_type', 'object_id']
 
     def __init__(self, *args, **kwargs):
         obj = kwargs.pop('obj', None)
@@ -20,3 +46,6 @@ class DocumentUploadForm(forms.ModelForm):
         if next:
             self.fields['next'] = forms.CharField(widget=forms.HiddenInput())
             self.initial['next'] = next
+
+# TODO : templates: moves them to the correct directory
+# TODO : test E2E
