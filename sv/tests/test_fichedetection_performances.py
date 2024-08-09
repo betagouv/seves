58ed43df4b61923dce0221099969ea4f3dd98e58
Anto59290 <antonin59290@hotmail.com>
2024-08-09 14:52:51 +0200

    Améliorer et tester les perfs de la page fiche
    Le but de ce commit est de tester et d'améliorer les performances de la
    page de détails d'une fiche détection (et des autres types de fiche dans
    le futur pour les objects communs).
    
    - Mise en place de tests pour vérifier la "scalabilité" de la page en
      termes de requêtes SQL face a l'accroissement du nombre d'objets
    - Diverses améliorations de performances : prefetch related, select
      related et "cache" de l'objet courant dans la vue de détails.
    
    En local sur une page test (plusieurs messages, documents, lieux,
    prélévements, etc.) on passe de 137 requêtes à 25 requêtes.

---
+++ sv/tests/test_fichedetection_performances.py
@@ -0,0 +1,77 @@
+import pytest
+from model_bakery import baker
+
+from core.models import Message, Document
+from sv.models import FicheDetection, Lieu, Prelevement
+
+BASE_NUM_QUERIES = 9  # Please note a first call is made without assertion to warm up any possible cache
+
+
+@pytest.mark.django_db
+def test_empty_fiche_detection_performances(client, django_assert_num_queries):
+    fiche = baker.make(FicheDetection)
+    client.get(fiche.get_absolute_url())
+
+    with django_assert_num_queries(BASE_NUM_QUERIES):
+        client.get(fiche.get_absolute_url())
+
+
+@pytest.mark.django_db
+def test_fiche_detection_performances_with_messages_from_same_user(
+    client, django_assert_num_queries, mocked_authentification_user
+):
+    fiche = baker.make(FicheDetection)
+    client.get(fiche.get_absolute_url())
+
+    baker.make(Message, content_object=fiche, sender=mocked_authentification_user.agent.contact_set.get())
+    with django_assert_num_queries(BASE_NUM_QUERIES + 6):
+        client.get(fiche.get_absolute_url())
+
+    baker.make(Message, content_object=fiche, sender=mocked_authentification_user.agent.contact_set.get(), _quantity=3)
+
+    with django_assert_num_queries(BASE_NUM_QUERIES + 6):
+        response = client.get(fiche.get_absolute_url())
+
+    assert len(response.context["message_list"]) == 4
+
+
+@pytest.mark.django_db
+def test_fiche_detection_performances_with_lieux(client, django_assert_num_queries):
+    fiche = baker.make(FicheDetection)
+    client.get(fiche.get_absolute_url())
+
+    with django_assert_num_queries(BASE_NUM_QUERIES):
+        client.get(fiche.get_absolute_url())
+
+    baker.make(Lieu, fiche_detection=fiche, _quantity=3)
+    with django_assert_num_queries(BASE_NUM_QUERIES):
+        client.get(fiche.get_absolute_url())
+
+
+@pytest.mark.django_db
+def test_fiche_detection_performances_with_document(client, django_assert_num_queries):
+    fiche = baker.make(FicheDetection)
+    client.get(fiche.get_absolute_url())
+
+    with django_assert_num_queries(BASE_NUM_QUERIES):
+        client.get(fiche.get_absolute_url())
+
+    baker.make(Document, content_object=fiche, _quantity=3, _create_files=True)
+    with django_assert_num_queries(BASE_NUM_QUERIES + 1):
+        client.get(fiche.get_absolute_url())
+
+
+@pytest.mark.django_db
+def test_fiche_detection_performances_with_prelevement(client, django_assert_num_queries):
+    fiche = baker.make(FicheDetection)
+    client.get(fiche.get_absolute_url())
+
+    with django_assert_num_queries(BASE_NUM_QUERIES):
+        client.get(fiche.get_absolute_url())
+
+    for _ in range(0, 3):
+        lieu = baker.make(Lieu, fiche_detection=fiche)
+        baker.make(Prelevement, lieu=lieu)
+
+    with django_assert_num_queries(BASE_NUM_QUERIES):
+        client.get(fiche.get_absolute_url())
