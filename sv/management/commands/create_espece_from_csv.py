06184f7b738060d5be878d42e33b1dc7cdf9f0b2
Anto59290 <antonin59290@hotmail.com>
2024-09-10 14:53:30 +0200

    Permet d'importer les espèces depuis un fichier CSV

---
+++ sv/management/commands/create_espece_from_csv.py
@@ -0,0 +1,19 @@
+import csv
+
+from django.core.management.base import BaseCommand
+from sv.models import EspeceEchantillon
+
+
+class Command(BaseCommand):
+    help = "Permet l'import des espèces et des code OEPP depuis un fichier CSV."
+
+    def add_arguments(self, parser):
+        parser.add_argument("--file", type=str)
+
+    def handle(self, *args, **options):
+        with open(options["file"]) as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=";")
+            especes = [
+                EspeceEchantillon(code_oepp=row["vegetalCodeOeppLb"], libelle=row["vegetalCourtLb"]) for row in reader
+            ]
+            EspeceEchantillon.objects.bulk_create(especes)
