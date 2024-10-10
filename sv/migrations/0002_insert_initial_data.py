fc359f75f2c150dd66cc39cfac954aa6e711a7fb
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-10-10 10:11:13 +0100

    Dans la catégorie types exploitant : 'batiment' --> 'bâtiment'

--- sv/migrations/0002_insert_initial_data.py
+++ sv/migrations/0002_insert_initial_data.py
@@ -910,8 +910,8 @@ TYPES_EXPLOITANT = [
     "Plateforme logistique",
     "Site de stockage de bois",
     "Serre",
-    "Batiment privé",
-    "Batiment public",
+    "Bâtiment privé",
+    "Bâtiment public",
     "Autre (à préciser dans le fil de suivi)",
     "Sans objet",
 ]
