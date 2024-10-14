211ce7213d377f27221badbdd721d0d07f980c24
Anto59290 <antonin59290@hotmail.com>
2024-10-14 17:06:08 +0200

    Utiliser Django Filters pour la recherche de fiche
    Ce commit permet d'utiliser Django Filters dans la rechercher des fiches
    détection afin de rendre plus simple la mise en place de la recherche
    sur les fiches Zone delimitées.

---
+++ sv/filters.py
@@ -0,0 +1,49 @@
+import django_filters
+
+from core.forms import DSFRForm
+from .models import FicheDetection, Region, OrganismeNuisible, Etat
+from django.forms.widgets import DateInput, TextInput
+
+
+class FicheDetectionFilter(django_filters.FilterSet):
+    numero = django_filters.CharFilter(
+        method="filter_numero",
+        label="Numéro",
+        widget=TextInput(attrs={"pattern": "^[0-9]{4}\\.[0-9]+$", "title": "Format attendu : ANNEE.NUMERO"}),
+    )
+    lieux__departement__region = django_filters.ModelChoiceFilter(label="Région", queryset=Region.objects.all())
+    organisme_nuisible = django_filters.ModelChoiceFilter(label="Organisme", queryset=OrganismeNuisible.objects.all())
+    start_date = django_filters.DateFilter(
+        field_name="date_creation__date",
+        lookup_expr="gte",
+        label="Période du",
+        widget=DateInput(attrs={"type": "date"}),
+    )
+    end_date = django_filters.DateFilter(
+        field_name="date_creation__date", lookup_expr="lte", label="Au", widget=DateInput(attrs={"type": "date"})
+    )
+    etat = django_filters.ModelChoiceFilter(label="État", queryset=Etat.objects.all())
+
+    class Meta:
+        model = FicheDetection
+        fields = ["numero", "lieux__departement__region", "organisme_nuisible", "start_date", "end_date", "etat"]
+        form = DSFRForm
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if "numero" in self.data:
+            try:
+                _annee, _numero = map(int, self.data.get("numero").split("."))
+            except ValueError:
+                errors = self.errors.get("numero", [])
+                errors.append("Format 'numero' invalide. Il devrait être 'annee.numero'")
+                self.errors["numero"] = errors
+
+    def filter_numero(self, queryset, name, value):
+        if not value:
+            return queryset
+        try:
+            annee, numero = map(int, value.split("."))
+        except ValueError:
+            return queryset
+        return queryset.filter(numero__annee=annee, numero__numero=numero)
--- sv/static/sv/fichedetection_list.js
+++ sv/static/sv/fichedetection_list.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('search-form').addEventListener('reset', function (e) {
         e.preventDefault();
         this.elements['numero'].value = '';
-        this.elements['region'].value = '';
+        this.elements['lieux__departement__region'].value = '';
         this.elements['organisme_nuisible'].value = '';
-        this.elements['date_debut'].value = '';
-        this.elements['date_fin'].value = '';
+        this.elements['start_date'].value = '';
+        this.elements['end_date'].value = '';
         this.elements['etat'].value = '';
     });
 
