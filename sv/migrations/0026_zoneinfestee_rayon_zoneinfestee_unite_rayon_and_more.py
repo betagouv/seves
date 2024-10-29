26dab49d13ce350ee1957ec2be2cf71293ed629b
Anto59290 <antonin59290@hotmail.com>
2024-10-29 15:22:39 +0100

    Corriger une collision de migrations

--- sv/migrations/0026_zoneinfestee_rayon_zoneinfestee_unite_rayon_and_more.py
+++ sv/migrations/0026_zoneinfestee_rayon_zoneinfestee_unite_rayon_and_more.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("sv", "0024_laboratoireagree_is_active_and_more"),
+        ("sv", "0025_structurepreleveur_is_active"),
     ]
 
     operations = [
