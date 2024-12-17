9ef89b123e9eb748477ab01a9e2827429163e1c8
Julien Bouquillon <julien@bouquillon.com>
2024-12-17 10:45:44 +0100

    fix(sentry): reduce tracesSampleRate
    SEVES is sending +800k/events/day on the shared sentry instance. the production value should be around 0.1 or lower
    
    https://docs.sentry.io/platforms/javascript/tracing/#configure

--- seves/settings.py
+++ seves/settings.py
@@ -159,7 +159,7 @@ if SENTRY_DSN:
     sentry_sdk.init(
         dsn=SENTRY_DSN,
         integrations=[DjangoIntegration()],
-        traces_sample_rate=1.0,
+        traces_sample_rate=0.1,
     )
 
 
