817bade6994f85e965758d458e557a2f1c2b7f4d
alan <alanzirek@gmail.com>
2025-01-09 20:24:43 +0100

    Ajout d'un date picker sur la date de prélèvement (formulaire Prélèvement de la fiche détection)

--- sv/forms.py
+++ sv/forms.py
@@ -150,6 +150,7 @@ class PrelevementForm(DSFRForm, WithDataRequiredConversionMixin, forms.ModelForm
                 }
             ),
             "laboratoire": SelectWithAttributeField,
+            "date_prelevement": forms.DateInput(attrs={"type": "date"}, format="%Y-%m-%d"),
         }
 
     def __init__(self, *args, **kwargs):
