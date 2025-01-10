b8c17027edea8380068cdafb50f6654cfd88f604
alan <alanzirek@gmail.com>
2025-01-10 11:41:56 +0100

    Gestion du swith détail/synthèse sur la page détail de la fiche évènement
    ajoute la possibilité de basculer entre une vue détaillée et une vue synthétique sur sur la page détail d'une fiche évènement

--- sv/static/sv/evenement_details.js
+++ sv/static/sv/evenement_details.js
@@ -12,8 +12,21 @@ function showOnlyActionsForDetection(detectionId){
     )
 }
 
+function toggleViewMode(event) {
+    const detailContent = document.getElementById('detail-content');
+    const viewMode = event.target.value;
+    detailContent.classList.toggle('fr-hidden', viewMode === 'synthese');
+}
+
+function initViewModeButtons() {
+    const detailBtn = document.getElementById('detail-btn');
+    const syntheseBtn = document.getElementById('synthese-btn');
+    detailBtn.addEventListener('change', toggleViewMode);
+    syntheseBtn.addEventListener('change', toggleViewMode);
+}
 
 document.addEventListener('DOMContentLoaded', function() {
+    initViewModeButtons();
     document.querySelectorAll(".no-tab-look .fr-tabs__panel").forEach(element =>{
         element.addEventListener('dsfr.conceal', event=>{
             const tabId = event.explicitOriginalTarget.getAttribute("id").replace("tabpanel-", "").replace("-panel", "")
