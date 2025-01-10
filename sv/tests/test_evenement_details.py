3bb2ffbbdb8fe3939902fcd6e7ff4d7411882157
alan <alanzirek@gmail.com>
2025-01-10 11:41:56 +0100

    Gestion du swith détail/synthèse sur la page détail de la fiche évènement
    ajoute la possibilité de basculer entre une vue détaillée et une vue synthétique sur sur la page détail d'une fiche évènement

--- sv/tests/test_evenement_details.py
+++ sv/tests/test_evenement_details.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import expect, Page
 
 from core.models import Visibilite
-from sv.factories import EvenementFactory, FicheZoneFactory
+from sv.factories import EvenementFactory, FicheZoneFactory, FicheDetectionFactory
 
 
 def test_cant_add_zone_if_already_one(live_server, page: Page):
@@ -24,3 +24,21 @@ def test_can_publish_evenement(live_server, page: Page):
     page.get_by_role("button", name="Actions").click()
     publish_btn = page.get_by_text("Publier l'événement", exact=True)
     expect(publish_btn).not_to_be_visible()
+
+
+def test_detail_synthese_switch(live_server, page):
+    evenement = EvenementFactory()
+    FicheDetectionFactory(evenement=evenement)
+    page.goto(f"{live_server.url}{evenement.get_absolute_url()}")
+
+    detail_radio = page.locator("#detail-btn")
+    expect(detail_radio).to_be_checked()
+    detail_content = page.locator("#detail-content")
+    expect(detail_content).to_be_visible()
+
+    synthese_radio = page.locator("#synthese-btn")
+    synthese_radio.click(force=True)
+    expect(detail_content).to_be_hidden()
+
+    detail_radio.click(force=True)
+    expect(detail_content).to_be_visible()
