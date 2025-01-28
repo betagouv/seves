f09f3ec37051e6811019ffa6f36cb4cb22eca368
alan <alanzirek@gmail.com>
2025-01-28 11:42:40 +0100

    Navigation depuis liste des fiches vers onglets zone et détection
    - Ajoute redirection vers l'onglet zone lors d'un clic sur une zone dans le tableau
    - Ajoute redirection vers l'onglet détection avec sélection lors d'un clic sur une détection

--- sv/display.py
+++ sv/display.py
@@ -31,7 +31,7 @@ class DisplayedFiche:
             etat=fiche.evenement.etat,
             readable_etat=fiche.evenement.get_etat_display(),
             communes_list=[],
-            get_absolute_url=fiche.get_absolute_url(),
+            get_absolute_url=fiche.get_absolute_url() + "#tabpanel-zone-panel",
             numero_evenement=str(fiche.evenement.numero) if fiche.evenement.numero else "non attribué",
             nb_related_objects=fiche.nb_fiches_detection,
         )
@@ -49,7 +49,7 @@ class DisplayedFiche:
             readable_etat=fiche.evenement.get_etat_display(),
             visibilite=str(fiche.evenement.visibilite),
             communes_list=fiche.lieux_list_with_commune,
-            get_absolute_url=fiche.get_absolute_url(),
+            get_absolute_url=fiche.get_absolute_url() + f"?detection={fiche.pk}",
             numero_evenement=str(fiche.evenement.numero) if fiche.evenement.numero else "non attribué",
             nb_related_objects=1 if fiche.evenement.fiche_zone_delimitee_id else 0,
         )
--- sv/tests/test_fichedetection_list.py
+++ sv/tests/test_fichedetection_list.py
@@ -1,7 +1,9 @@
+import re
+
 from django.urls import reverse
 from playwright.sync_api import Page, expect
 
-from sv.factories import FicheDetectionFactory, LieuFactory
+from sv.factories import FicheDetectionFactory, LieuFactory, EvenementFactory, FicheZoneFactory
 
 
 def test_commune_column_with_multiple_communes(live_server, page: Page):
@@ -36,3 +38,25 @@ def test_commune_column_without_lieu(live_server, page: Page):
     FicheDetectionFactory()
     page.goto(f"{live_server}{reverse('fiche-liste')}")
     expect(page.get_by_text("nc.", exact=True)).to_be_visible()
+
+
+def test_click_on_detection_in_table_redirects_to_detection_tab(live_server, page):
+    evenement = EvenementFactory()
+    _, detection_2, _ = FicheDetectionFactory.create_batch(3, evenement=evenement)
+
+    page.goto(f"{live_server}{reverse('fiche-liste')}")
+    page.get_by_test_id(f"{detection_2.numero}").click()
+
+    expect(page.get_by_role("tab", name=f"{detection_2.numero}")).to_be_visible()
+    expect(page.get_by_role("tab", name=f"{detection_2.numero}")).to_have_class(re.compile(r"(^|\s)selected($|\s)"))
+
+
+def test_click_on_zone_in_table_redirects_to_zone_tab(live_server, page):
+    fiche_zone = FicheZoneFactory()
+    evenement = EvenementFactory(fiche_zone_delimitee=fiche_zone)
+
+    page.goto(f"{live_server}{reverse('fiche-liste')}?type_fiche=zone")
+    page.get_by_role("link", name=f"{evenement.numero}").click()
+
+    expect(page.get_by_role("tab", name="Zone")).to_be_visible()
+    expect(page.get_by_role("heading", name="Zone tampon")).to_be_visible()
