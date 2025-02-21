d04833c844fce0534e87aa6d85e634c7b5fef905
alan <alanzirek@gmail.com>
2025-02-21 17:06:24 +0100

    Arriver en haut de la page quand on revient sur l’évènement

--- sv/views.py
+++ sv/views.py
@@ -488,7 +488,7 @@ class FicheZoneDelimiteeCreateView(WithFormErrorsAsMessagesMixin, CreateView):
     context_object_name = "fiche"
 
     def get_success_url(self):
-        return reverse("evenement-details", args=[self.object.evenement.numero]) + "#tabpanel-zone-panel"
+        return reverse("evenement-details", args=[self.object.evenement.numero]) + "?tab=zone"
 
     def dispatch(self, request, *args, **kwargs):
         try:
@@ -595,7 +595,7 @@ class FicheZoneDelimiteeUpdateView(
     context_object_name = "fiche"
 
     def get_success_url(self):
-        return self.get_object().get_absolute_url() + "#tabpanel-zone-panel"
+        return self.get_object().get_absolute_url() + "?tab=zone"
 
     def test_func(self) -> bool | None:
         return self.get_object().evenement.can_user_access(self.request.user)
