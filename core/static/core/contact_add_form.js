2bd969a04c4a483153e842a148e828cb7eb34da1
alan <alanzirek@gmail.com>
2025-03-07 23:35:42 +0100

    Nouveaux formulaires pour ajout de structure ou agent dans Contacts

---
+++ core/static/core/contact_add_form.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const options = {
+        itemSelectText: '',
+        classNames: {
+            containerInner: 'fr-select',
+        },
+        removeItemButton: true,
+    };
+    const select_contacts_structures = document.getElementById('id_contacts_structures');
+    const select_contacts_agents = document.getElementById('id_contacts_agents');
+
+    select_contacts_structures.style.visibility = 'visible';
+    select_contacts_agents.style.visibility = 'visible';
+
+    new Choices(select_contacts_structures, options);
+    new Choices(select_contacts_agents, options);
+});
