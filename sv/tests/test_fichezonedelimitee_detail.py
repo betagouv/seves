2e26af288061dc0f5242ff728e1005bf139b071c
Anto59290 <antonin59290@hotmail.com>
2025-04-03 18:03:28 +0200

    Remplace baker par des factories

--- sv/tests/test_fichezonedelimitee_detail.py
+++ sv/tests/test_fichezonedelimitee_detail.py
@@ -1,19 +1,14 @@
-from model_bakery import baker
 from playwright.sync_api import Page, expect
 
-from sv.factories import EvenementFactory, FicheZoneFactory
-from sv.models import ZoneInfestee, FicheDetection
+from sv.factories import EvenementFactory, FicheZoneFactory, ZoneInfesteeFactory, FicheDetectionFactory
 
 
 def test_fichezonedelimitee_with_zoneinfestee_detail(live_server, page: Page, mocked_authentification_user):
     fiche_zone_delimitee = FicheZoneFactory()
     evenement = EvenementFactory(fiche_zone_delimitee=fiche_zone_delimitee)
-    zone_infestee_1 = baker.make(ZoneInfestee, fiche_zone_delimitee=fiche_zone_delimitee, _fill_optional=True)
-    fiche_detection_fiche_zone_delimitee = baker.make(
-        FicheDetection,
-        hors_zone_infestee=fiche_zone_delimitee,
-    )
-    fiche_detection_zone_infestee_1 = baker.make(FicheDetection, zone_infestee=zone_infestee_1)
+    zone_infestee_1 = ZoneInfesteeFactory(fiche_zone_delimitee=fiche_zone_delimitee)
+    fiche_detection_fiche_zone_delimitee = FicheDetectionFactory(hors_zone_infestee=fiche_zone_delimitee)
+    fiche_detection_zone_infestee_1 = FicheDetectionFactory(zone_infestee=zone_infestee_1)
     page.goto(f"{live_server.url}{evenement.get_absolute_url()}")
     page.get_by_role("tab", name="Zone").click()
 
@@ -27,8 +22,12 @@ def test_fichezonedelimitee_with_zoneinfestee_detail(live_server, page: Page, mo
     expect(page.get_by_role("link", name=f"{str(fiche_detection_fiche_zone_delimitee.numero)}")).to_be_visible()
     expect(page.get_by_text(f"{zone_infestee_1.nom}")).to_be_visible()
     expect(page.get_by_text(f"{zone_infestee_1.get_caracteristique_principale_display()}")).to_be_visible()
-    expect(page.get_by_text(f"{zone_infestee_1.rayon} {zone_infestee_1.unite_rayon}")).to_be_visible()
     expect(
-        page.get_by_text(f"{zone_infestee_1.surface_infestee_totale} {zone_infestee_1.unite_surface_infestee_totale}")
+        page.get_by_text(f"{str(zone_infestee_1.rayon).rstrip('0').rstrip('.')} {zone_infestee_1.unite_rayon}")
+    ).to_be_visible()
+    expect(
+        page.get_by_text(
+            f"{str(zone_infestee_1.surface_infestee_totale).rstrip('0').rstrip('.')} {zone_infestee_1.unite_surface_infestee_totale}"
+        )
     ).to_be_visible()
     expect(page.get_by_role("link", name=f"{str(fiche_detection_zone_infestee_1.numero)}")).to_be_visible()
