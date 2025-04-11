a4aed354c72858e0ec8c8e971e07b1e2fbd4b6d8
alan <alanzirek@gmail.com>
2025-04-11 23:34:58 +0200

    Gestion des droits d'accès par domaine (SV, SSA)
    - Modification UI “gestion des droits d’accès” pour activer/desactiver l'accès à SV et/ou SSA.
    - Implémente une règle de sécurité supplémentaire: un agent
    ne peut gérer que les droits des domaines auxquels il appartient.
    Par exemple, un agent du groupe SV ne peut attribuer/retirer que
    les droits SV, même s'il possède le groupe access_admin,
    - Modifications pour amélioration performances (requêtes SQL).

--- account/forms.py
+++ account/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.contrib.auth import get_user_model
 
-from core.fields import DSFRToogle
-from core.form_mixins import DSFRForm
+from core.fields import DSFRCheckboxInput
+from core.forms import DSFRForm
 
 User = get_user_model()
 
@@ -10,9 +10,23 @@ User = get_user_model()
 class UserPermissionForm(DSFRForm, forms.Form):
     def __init__(self, **kwargs):
         users = kwargs.pop("users")
+        can_manage_sv = kwargs.pop("can_manage_sv", False)
+        can_manage_ssa = kwargs.pop("can_manage_ssa", False)
         super().__init__(**kwargs)
         for user in users:
-            self.fields[f"user_{user.pk}"] = forms.BooleanField(
-                required=False,
-                widget=DSFRToogle(attrs={"class": "fr-toggle__input", "label": str(user.agent), "id": user.pk}),
-            )
+            if can_manage_sv:
+                self.fields[f"sv_{user.pk}"] = forms.BooleanField(
+                    required=False,
+                    widget=DSFRCheckboxInput(
+                        attrs={"id": f"sv_{user.pk}"},
+                        label="",
+                    ),
+                )
+            if can_manage_ssa:
+                self.fields[f"ssa_{user.pk}"] = forms.BooleanField(
+                    required=False,
+                    widget=DSFRCheckboxInput(
+                        attrs={"id": f"ssa_{user.pk}"},
+                        label="",
+                    ),
+                )
