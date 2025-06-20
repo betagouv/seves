aa6b25416289f7ad2a61490518f8eb062e58470e
Anto59290 <antonin59290@hotmail.com>
2025-06-20 13:28:11 +0200

    Mets à jour le compteur de filtres au chargement de la page

--- ssa/static/ssa/evenement_produit_list.js
+++ ssa/static/ssa/evenement_produit_list.js
@@ -60,26 +60,32 @@ document.addEventListener('DOMContentLoaded', function() {
     function disableCheckboxIfNeeded(){
         document.querySelector("#id_with_free_links").disabled = !document.querySelector("#id_numero").value
     }
+
+    function updateFilterCounter(){
+        let filledFields = [...document.getElementById("sidebar").querySelectorAll('input, select')]
+        filledFields = filledFields.filter(el => el.value.trim() !== '');
+
+        if (filledFields.length === 0){
+            document.getElementById("more-filters-btn-counter").classList.add("fr-hidden")
+        } else {
+            document.getElementById("more-filters-btn-counter").innerText = filledFields.length
+            document.getElementById("more-filters-btn-counter").classList.remove("fr-hidden")
+        }
+    }
+
     document.querySelector(".clear-btn").addEventListener("click", clearSidebarFilters)
     document.querySelector(".add-btn").addEventListener("click", addSidebarFilters)
     document.querySelector("#id_numero").addEventListener("input", disableCheckboxIfNeeded)
     disableCheckboxIfNeeded()
     setupCategorieProduit()
     setupCategorieDanger()
+    updateFilterCounter()
 
     const sidebarClosingObserver = new MutationObserver((mutations) => {
         mutations.forEach(mutation => {
             if (mutation.type !== "attributes" && mutation.attributeName !== "class") return;
             if (!mutation.target.classList.contains("open")){
-                let filledFields = [...document.getElementById("sidebar").querySelectorAll('input, select')]
-                filledFields = filledFields.filter(el => el.value.trim() !== '');
-
-                if (filledFields.length === 0){
-                    document.getElementById("more-filters-btn-counter").classList.add("fr-hidden")
-                } else {
-                    document.getElementById("more-filters-btn-counter").innerText = filledFields.length
-                    document.getElementById("more-filters-btn-counter").classList.remove("fr-hidden")
-                }
+                updateFilterCounter()
             }
         });
     });
