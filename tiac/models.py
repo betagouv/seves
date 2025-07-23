fd23adfa0ae3406a1cb79130642de00a9b22e8e7
Christophe Henry <contact@c-henry.fr>
2025-07-23 10:56:53 +0200

    chore(tiac): model for simple event

---
+++ tiac/models.py
@@ -0,0 +1,50 @@
+import reversion
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+from core.mixins import (
+    AllowsSoftDeleteMixin,
+    WithContactPermissionMixin,
+    WithEtatMixin,
+    WithNumeroMixin,
+    WithFreeLinkIdsMixin,
+)
+from core.models import Structure
+from tiac.constants import ModaliteDeclarationEvenement, EvenementOrigin, EvenementFollowUp
+
+
+@reversion.register()
+class EvenementSimple(
+    AllowsSoftDeleteMixin,
+    WithContactPermissionMixin,
+    WithEtatMixin,
+    WithNumeroMixin,
+    WithFreeLinkIdsMixin,
+    models.Model,
+):
+    createur = models.ForeignKey(Structure, on_delete=models.PROTECT, verbose_name="Structure créatrice")
+    date_creation = models.DateTimeField(auto_now_add=True, verbose_name="Date de création")
+    date_reception = models.DateTimeField(verbose_name="Date de réception à la DD(ETS)PP")
+    evenement_origin = models.IntegerField(choices=EvenementOrigin.choices, verbose_name="Signalement déclaré par")
+    modalites_declaration = models.IntegerField(
+        choices=ModaliteDeclarationEvenement.choices,
+        default=ModaliteDeclarationEvenement.SIGNAL_CONSO,
+        verbose_name="Modalités de déclaration",
+    )
+    contenu = models.TextField(verbose_name="Contenu du signalement")
+    notify_ars = models.BooleanField(default=False, verbose_name="ARS informée")
+    nb_sick_persons = models.IntegerField(verbose_name="Nombre de malades total")
+
+    follow_up = models.IntegerField(
+        choices=EvenementFollowUp.choices, default=EvenementFollowUp.NONE, verbose_name="Suite donnée par la DD"
+    )
+
+    etablissements = models.ManyToManyField("ssa.Etablissement", verbose_name="Établissements impliqués")
+
+
+class EvenementsLies(models.Model):
+    evenement = models.ForeignKey(EvenementSimple, on_delete=models.CASCADE, related_name="+")
+    releted_evenements_ct = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name="+")
+    releted_evenements_id = models.BigIntegerField()
+    evenements_lies = GenericForeignKey("releted_evenements_ct", "releted_evenements_id")
