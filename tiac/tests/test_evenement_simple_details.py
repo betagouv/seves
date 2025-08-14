f249020e2e9597225929ae4abda3bbffb2a94ab7
Anto59290 <antonin59290@hotmail.com>
2025-08-14 14:57:21 +0200

    Add latest version information on TIAC Evenement Simple

--- tiac/tests/test_evenement_simple_details.py
+++ tiac/tests/test_evenement_simple_details.py
@@ -16,6 +16,7 @@ def test_evenement_simple_detail_page_content(live_server, page: Page):
     details_page.navigate(evenement)
 
     assert details_page.title.text_content() == f"Événement {evenement.numero}"
+    assert "Dernière mise à jour" in details_page.last_modification.text_content()
 
     expect(details_page.context_block.get_by_text(str(evenement.createur), exact=True)).to_be_visible()
     expect(
