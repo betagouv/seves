387b3df975b18453f31f84db6a46f7c0d367aa65
Anto59290 <antonin59290@hotmail.com>
2025-08-15 10:21:52 +0200

    Allow to close (cloturer) an EvenementSimple in TIAC

--- tiac/tests/test_evenement_simple_details_actions.py
+++ tiac/tests/test_evenement_simple_details_actions.py
@@ -1,8 +1,11 @@
-from playwright.sync_api import expect
+from playwright.sync_api import expect, Page
 
+from core.factories import ContactStructureFactory
+from core.models import Structure, Contact
 from tiac.factories import EvenementSimpleFactory
 from tiac.models import EvenementSimple
 from .pages import EvenementSimpleDetailsPage
+from core.constants import AC_STRUCTURE, MUS_STRUCTURE
 
 
 def test_can_delete_evenement_simple(live_server, page):
@@ -16,3 +19,21 @@ def test_can_delete_evenement_simple(live_server, page):
 
     assert EvenementSimple.objects.count() == 0
     assert EvenementSimple._base_manager.get().pk == evenement.pk
+
+
+def test_can_cloturer_evenement_produit(live_server, page: Page, mocked_authentification_user):
+    ac_structure = Structure.objects.create(niveau1=AC_STRUCTURE, niveau2=MUS_STRUCTURE, libelle=MUS_STRUCTURE)
+    contact = Contact.objects.create(structure=ac_structure)
+    evenement = EvenementSimpleFactory(etat=EvenementSimple.Etat.EN_COURS)
+    mocked_authentification_user.agent.structure = ac_structure
+    evenement.contacts.add(contact)
+    evenement.contacts.add(ContactStructureFactory(structure=evenement.createur))
+
+    details_page = EvenementSimpleDetailsPage(page, live_server.url)
+    details_page.navigate(evenement)
+    details_page.cloturer()
+
+    evenement.refresh_from_db()
+    assert evenement.etat == EvenementSimple.Etat.CLOTURE
+    expect(page.get_by_text("Clôturé", exact=True)).to_be_visible()
+    expect(page.get_by_text(f"L'événement n°{evenement.numero} a bien été clôturé.")).to_be_visible()
