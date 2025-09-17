5f61f08daefe9d081d58afa4dbf79358fff931b5
Anto59290 <antonin59290@hotmail.com>
2025-09-17 11:13:37 +0200

    Debug login

--- core/auth_views.py
+++ core/auth_views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.conf import settings
 from mozilla_django_oidc.views import OIDCAuthenticationCallbackView
+import logging
 
 User = get_user_model()
+logger = logging.getLogger(__name__)
 
 
 class LoginView(TemplateView):
@@ -18,7 +20,10 @@ def logout(request):
 
 class CustomOIDCAuthenticationCallbackView(OIDCAuthenticationCallbackView):
     def login_failure(self):
+        logger.info("In login failure")
         if self.user and not self.user.is_active:
+            logger.info(f"In login failure {self.user}")
+            logger.info(f"In login failure {self.user.is_active}")
             if hasattr(self.user, "agent") and hasattr(self.user.agent, "structure"):
                 users_in_structure = User.objects.filter(agent__structure=self.user.agent.structure, is_active=True)
                 users_in_structure = users_in_structure.select_related("agent__structure")
