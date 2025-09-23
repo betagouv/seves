b68f7e22f7d5ec8c4643c4e6096fa2827e27af11
Anto59290 <antonin59290@hotmail.com>
2025-09-23 15:31:47 +0200

    Allow for a tiac list view mixed between two models
    With the use of `django-querysetsequence` we can re-use most of the
    existing code to change the evenement simple list view to a common list
    view.
    
    - A few tweaks needed on the view to make it work with Django list view
      (context_object_name and get_template_names, mostly because Django
    cannot inspect to get the model name from the Meta class)
    - A tweak needed in the filterset to remove an assertion on the type of
      the object

--- tiac/tests/test_evenement_list.py
+++ tiac/tests/test_evenement_list.py
@@ -1,15 +1,15 @@
 from playwright.sync_api import Page
 
-from tiac.factories import EvenementSimpleFactory
-from tiac.models import EvenementSimple
+from tiac.factories import EvenementSimpleFactory, InvestigationTiacFactory
+from tiac.models import EvenementSimple, InvestigationTiac, TypeEvenement
 from tiac.tests.pages import EvenementListPage
 
 
 def test_list_table_order(live_server, mocked_authentification_user, page: Page):
     EvenementSimpleFactory(numero_annee=2025, numero_evenement=2)
     EvenementSimpleFactory(numero_annee=2025, numero_evenement=1)
-    EvenementSimpleFactory(numero_annee=2025, numero_evenement=22)
-    EvenementSimpleFactory(numero_annee=2024, numero_evenement=22)
+    InvestigationTiacFactory(numero_annee=2025, numero_evenement=22)
+    InvestigationTiacFactory(numero_annee=2024, numero_evenement=22)
     search_page = EvenementListPage(page, live_server.url)
     search_page.navigate()
 
@@ -19,7 +19,7 @@ def test_list_table_order(live_server, mocked_authentification_user, page: Page)
     assert search_page.numero_cell(line_index=4).text_content() == "T-2024.22"
 
 
-def test_row_content(live_server, mocked_authentification_user, page: Page):
+def test_row_content_evenement_simple(live_server, mocked_authentification_user, page: Page):
     evenement: EvenementSimple = EvenementSimpleFactory()
     search_page = EvenementListPage(page, live_server.url)
     search_page.navigate()
@@ -30,6 +30,22 @@ def test_row_content(live_server, mocked_authentification_user, page: Page):
     assert search_page.etablissement_cell().text_content() == "-"
     assert search_page.malades_cell().text_content() == str(evenement.nb_sick_persons)
     assert search_page.type_cell().text_content() == f"Enr. simple / {evenement.get_follow_up_display()}"
+    assert search_page.conclusion_cell().text_content() == "-"
+    assert search_page.danger_cell().text_content() == "-"
+    assert search_page.etat_cell().text_content() == "Brouillon"
+
+
+def test_row_content_investigation_tiac(live_server, mocked_authentification_user, page: Page):
+    evenement: InvestigationTiac = InvestigationTiacFactory(type_evenement=TypeEvenement.INVESTIGATION_COORDONNEE)
+    search_page = EvenementListPage(page, live_server.url)
+    search_page.navigate()
+
+    assert search_page.numero_cell().text_content() == evenement.numero
+    assert search_page.createur_cell().text_content() == mocked_authentification_user.agent.structure.libelle
+    assert search_page.date_reception_cell().text_content() == evenement.date_reception.strftime("%d/%m/%Y")
+    assert search_page.etablissement_cell().text_content() == "-"
+    assert search_page.malades_cell().text_content() == "-"
+    assert search_page.type_cell().text_content() == "Invest. coord. / MUS informée"
     assert search_page.conclusion_cell().text_content() == "-"
     assert search_page.danger_cell().text_content() == "-"
     assert search_page.etat_cell().text_content() == "Brouillon"
