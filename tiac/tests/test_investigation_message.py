1c118b09f24bb24a54d57db404bcbbcece0078a4
Anto59290 <antonin59290@hotmail.com>
2025-10-06 17:10:03 +0200

    Improve / refacto test on messages
    - First issue the generic_test_can_update_draft_point_situation test was
      used in the test that was meant for the demande d'intervention.
    - Second issue, each object needs a different setup (for example the
      demande d'intervention need a Structure as a contact not an Agent,
    this is not guaranted by the factory). Splitting into different tests
    just looks easier.

--- tiac/tests/test_investigation_message.py
+++ tiac/tests/test_investigation_message.py
@@ -1,12 +1,9 @@
-import pytest
 from playwright.sync_api import Page
 
-from core.models import Message
 from core.tests.generic_tests.messages import (
     generic_test_can_add_and_see_message_without_document,
     generic_test_can_update_draft_note,
     generic_test_can_update_draft_point_situation,
-    generic_test_can_send_draft_element_suivi,
     generic_test_can_finaliser_draft_note,
     generic_test_can_send_draft_fin_suivi,
     generic_test_can_only_see_own_document_types_in_message_form,
@@ -14,6 +11,10 @@ from core.tests.generic_tests.messages import (
     generic_test_only_displays_app_contacts,
     generic_test_cant_see_drafts_from_other_users,
     generic_test_structure_show_only_one_entry_in_select,
+    generic_test_can_update_draft_demande_intervention,
+    generic_test_can_send_draft_message,
+    generic_test_can_send_draft_demande_intervention,
+    generic_test_can_send_draft_point_de_situation,
 )
 from tiac.factories import InvestigationTiacFactory
 from tiac.models import InvestigationTiac
@@ -41,10 +42,12 @@ def test_can_update_draft_point_situation(live_server, page: Page, mocked_authen
     )
 
 
-def test_can_update_draft_demande_intervention(live_server, page: Page, mocked_authentification_user, mailoutbox):
+def test_can_update_draft_demande_intervention(
+    live_server, page: Page, choice_js_fill, mocked_authentification_user, mailoutbox
+):
     evenement = InvestigationTiacFactory(etat=InvestigationTiac.Etat.EN_COURS)
-    generic_test_can_update_draft_point_situation(
-        live_server, page, mocked_authentification_user, evenement, mailoutbox
+    generic_test_can_update_draft_demande_intervention(
+        live_server, page, choice_js_fill, mocked_authentification_user, evenement, mailoutbox
     )
 
 
@@ -64,18 +67,22 @@ def test_can_update_draft_fin_suivi(live_server, page: Page, mocked_authentifica
     )
 
 
-@pytest.mark.parametrize(
-    "message_type",
-    [
-        Message.MESSAGE,
-        Message.POINT_DE_SITUATION,
-        Message.DEMANDE_INTERVENTION,
-    ],
-)
-def test_can_send_draft_element_suivi(live_server, page: Page, mocked_authentification_user, mailoutbox, message_type):
+def test_can_send_draft_message(live_server, page: Page, mocked_authentification_user, mailoutbox):
     evenement = InvestigationTiacFactory(etat=InvestigationTiac.Etat.EN_COURS)
-    generic_test_can_send_draft_element_suivi(
-        live_server, page, mocked_authentification_user, evenement, mailoutbox, message_type
+    generic_test_can_send_draft_message(live_server, page, mocked_authentification_user, evenement, mailoutbox)
+
+
+def test_can_send_draft_point_de_situation(live_server, page: Page, mocked_authentification_user, mailoutbox):
+    evenement = InvestigationTiacFactory(etat=InvestigationTiac.Etat.EN_COURS)
+    generic_test_can_send_draft_point_de_situation(
+        live_server, page, mocked_authentification_user, evenement, mailoutbox
+    )
+
+
+def test_can_send_draft_demande_intervention(live_server, page: Page, mocked_authentification_user, mailoutbox):
+    evenement = InvestigationTiacFactory(etat=InvestigationTiac.Etat.EN_COURS)
+    generic_test_can_send_draft_demande_intervention(
+        live_server, page, mocked_authentification_user, evenement, mailoutbox
     )
 
 
