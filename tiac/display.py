2b91106ef7312ff2269a2a3a9b058d32c2904dbb
Anto59290 <antonin59290@hotmail.com>
2025-10-08 10:34:17 +0200

    Add more data in the TIAC table list view
    - Add data about the etablissement if any
    - Add nb_sick_persons for InvestigationTiac

--- tiac/display.py
+++ tiac/display.py
@@ -13,6 +13,14 @@ class DisplayItem:
     type_evenement: str
     etat: str
     readable_etat: str
+    etablissement: str
+
+    @classmethod
+    def add_etablissement_data(cls, object):
+        if not (hasattr(object, "last_etablissement") and len(object.last_etablissement) > 0):
+            return "-"
+
+        return f"{object.last_etablissement[0].raison_sociale} {object.last_etablissement[0].commune}"
 
     @classmethod
     def from_evenement_simple(cls, evenement_simple: EvenementSimple):
@@ -26,19 +34,22 @@ class DisplayItem:
             type_evenement=f"Enr. simple / {evenement_simple.get_follow_up_display() if evenement_simple.get_follow_up_display() else '-'}",
             etat=evenement_simple.etat,
             readable_etat=evenement_simple.readable_etat,
+            etablissement=cls.add_etablissement_data(evenement_simple),
         )
 
     @classmethod
     def from_investigation_tiac(cls, investigation_tiac: InvestigationTiac):
+        nb_sick_persons = investigation_tiac.nb_sick_persons
         return cls(
             numero=investigation_tiac.numero,
             absolute_url=investigation_tiac.get_absolute_url(),
             createur=str(investigation_tiac.createur),
             date_reception=investigation_tiac.date_reception.strftime("%d/%m/%Y"),
-            nb_sick_persons="-",
+            nb_sick_persons=str(nb_sick_persons if nb_sick_persons is not None else "-"),
             type_evenement=investigation_tiac.type_evenement_display,
             etat=investigation_tiac.etat,
             readable_etat=investigation_tiac.readable_etat,
+            etablissement=cls.add_etablissement_data(investigation_tiac),
         )
 
     @classmethod
--- tiac/mixins.py
+++ tiac/mixins.py
@@ -1,6 +1,8 @@
+from django.db.models import Prefetch
+
 from core.mixins import WithOrderingMixin
 from tiac.filters import TiacFilter
-from tiac.models import EvenementSimple, InvestigationTiac
+from tiac.models import EvenementSimple, InvestigationTiac, Etablissement
 from queryset_sequence import QuerySetSequence
 
 
@@ -20,12 +22,20 @@ class WithFilteredListMixin(WithOrderingMixin):
     def get_raw_queryset(self):
         user = self.request.user
         contact = user.agent.structure.contact_set.get()
+        last_etablissement = Etablissement.objects.order_by("pk")[:1]
+        prefetch = Prefetch("etablissements", queryset=last_etablissement, to_attr="last_etablissement")
 
         evenement_simple_qs = (
-            EvenementSimple.objects.select_related("createur").get_user_can_view(user).with_fin_de_suivi(contact)
+            EvenementSimple.objects.select_related("createur")
+            .get_user_can_view(user)
+            .with_fin_de_suivi(contact)
+            .prefetch_related(prefetch)
         )
         investigation_qs = (
-            InvestigationTiac.objects.select_related("createur").get_user_can_view(user).with_fin_de_suivi(contact)
+            InvestigationTiac.objects.select_related("createur")
+            .get_user_can_view(user)
+            .with_fin_de_suivi(contact)
+            .prefetch_related(prefetch)
         )
 
         return QuerySetSequence(evenement_simple_qs, investigation_qs)
