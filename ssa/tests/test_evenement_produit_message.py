f36052efca64c7f1b08728f730112b3b1d5661e7
Anto59290 <antonin59290@hotmail.com>
2025-12-01 15:34:13 +0100

    Avoid sending deletion email when draft

--- ssa/tests/test_evenement_produit_message.py
+++ ssa/tests/test_evenement_produit_message.py
@@ -30,6 +30,7 @@ from core.tests.generic_tests.messages import (
     generic_test_can_update_draft_point_situation_in_new_tab,
     generic_test_can_send_draft_message_in_new_tab,
     generic_test_can_see_and_delete_documents_from_draft_message_in_new_tab,
+    generic_test_can_delete_my_own_draft_message,
 )
 from ssa.factories import EvenementProduitFactory
 from ssa.models import EvenementProduit
@@ -297,6 +298,13 @@ def test_can_add_message_in_new_tab_with_documents(live_server, page: Page, choi
 def test_can_delete_my_own_message(live_server, page: Page, mocked_authentification_user, mailoutbox):
     evenement_produit = EvenementProduitFactory(etat=EvenementProduit.Etat.EN_COURS)
     generic_test_can_delete_my_own_message(
+        live_server, page, evenement_produit, mocked_authentification_user, mailoutbox
+    )
+
+
+def test_can_delete_my_own_draft_message(live_server, page: Page, mocked_authentification_user, mailoutbox):
+    evenement_produit = EvenementProduitFactory(etat=EvenementProduit.Etat.EN_COURS)
+    generic_test_can_delete_my_own_draft_message(
         live_server, page, evenement_produit, mocked_authentification_user, mailoutbox
     )
 
--- tiac/tests/test_evenement_simple_message.py
+++ tiac/tests/test_evenement_simple_message.py
@@ -30,6 +30,7 @@ from core.tests.generic_tests.messages import (
     generic_test_can_update_draft_demande_intervention_in_new_tab,
     generic_test_can_send_draft_message_in_new_tab,
     generic_test_can_see_and_delete_documents_from_draft_message_in_new_tab,
+    generic_test_can_delete_my_own_draft_message,
 )
 from tiac.factories import EvenementSimpleFactory
 from tiac.models import EvenementSimple
@@ -252,10 +253,13 @@ def test_can_add_message_in_new_tab_with_documents(live_server, page: Page, choi
 
 
 def test_can_delete_my_own_message(live_server, page: Page, mocked_authentification_user, mailoutbox):
-    evenement_produit = EvenementSimpleFactory(etat=EvenementSimple.Etat.EN_COURS)
-    generic_test_can_delete_my_own_message(
-        live_server, page, evenement_produit, mocked_authentification_user, mailoutbox
-    )
+    evenement = EvenementSimpleFactory(etat=EvenementSimple.Etat.EN_COURS)
+    generic_test_can_delete_my_own_message(live_server, page, evenement, mocked_authentification_user, mailoutbox)
+
+
+def test_can_delete_my_own_draft_message(live_server, page: Page, mocked_authentification_user, mailoutbox):
+    evenement = EvenementSimpleFactory(etat=EvenementSimple.Etat.EN_COURS)
+    generic_test_can_delete_my_own_draft_message(live_server, page, evenement, mocked_authentification_user, mailoutbox)
 
 
 @override_flag("message_v2", active=True)
