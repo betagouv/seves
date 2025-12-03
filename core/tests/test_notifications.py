ee31c09c163eecd94a27b7408e7f51060e30e415
Anto59290 <antonin59290@hotmail.com>
2025-12-03 09:36:07 +0100

    Exclude contacts in fin de suivi when sending notification
    When we send a notification we want to make sure we don't send it to
    structures in FinDeSuivi and agents that are in a Structure that is in
    FinDeSuivi. This is True for all notification with the exception of the
    ones linked to a Message in FilDeSuivi. In this specific case if an
    agent or structure in FinDeSuivi is explicitly picked as a recipient we
    need to send the message / notification.
    
    Also introduce a proxy function to avoid the repetition of the layout /
    footer / subject.

---
+++ core/tests/test_notifications.py
@@ -0,0 +1,39 @@
+import pytest
+from django.db.models.signals import post_save
+
+from core.factories import RegionFactory, ContactAgentFactory, ContactStructureFactory
+from core.models import FinSuiviContact
+from core.notifications import send_as_seves
+from core.signals import fin_suivi_added
+
+
+@pytest.mark.django_db
+def test_notifications_are_filtered_with_fin_de_suivi(mailoutbox):
+    # We just need a dummy object to test the recipients
+    object = RegionFactory()
+    object.get_absolute_url = lambda: "foo"
+
+    contact = ContactAgentFactory()
+    contact_in_fin_suivi = ContactAgentFactory()
+
+    structure = ContactStructureFactory()
+    structure_in_fin_suivi = ContactStructureFactory(structure=contact_in_fin_suivi.agent.structure)
+
+    # We need to disconect the signal as Region object is not tracked with revision, so the fin_suivi_added signal
+    # makes no sense and crashes.
+    post_save.disconnect(fin_suivi_added, sender=FinSuiviContact)
+    FinSuiviContact.objects.create(content_object=object, contact=structure_in_fin_suivi)
+    post_save.connect(fin_suivi_added, sender=FinSuiviContact)
+
+    send_as_seves(
+        recipients=[contact.email, structure.email, contact_in_fin_suivi.email, structure_in_fin_suivi.email],
+        subject="Test",
+        message="Test",
+        html_message="Test",
+        object=object,
+    )
+
+    assert len(mailoutbox) == 1
+    mail = mailoutbox[0]
+
+    assert mail.to == [contact.email, structure.email]
