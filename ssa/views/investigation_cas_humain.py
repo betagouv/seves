3b9a8a17ca5a0ca549f6b309457df9ccfa58b5db
Christophe Henry <contact@c-henry.fr>
2025-12-15 16:13:08 +0100

    chore(ssa): move InvestigationCasHumainCreateView and InvestigationCasHumainUpdateView to investigation_cas_humain.py for consistancy

--- ssa/views/investigation_cas_humain.py
+++ ssa/views/investigation_cas_humain.py
@@ -1,20 +1,109 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.contenttypes.models import ContentType
-from django.http import Http404
-from django.views.generic import DetailView
+from django.forms import Media
+from django.http import Http404, HttpResponseRedirect
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from core.mixins import (
-    WithFreeLinksListInContextMixin,
-    WithClotureContextMixin,
+    WithAddUserContactsMixin,
     WithBlocCommunPermission,
-    WithDocumentListInContextMixin,
-    WithDocumentUploadFormMixin,
-    WithMessageMixin,
+    WithClotureContextMixin,
     WithContactFormsInContextMixin,
     WithContactListInContextMixin,
+    WithDocumentListInContextMixin,
+    WithDocumentUploadFormMixin,
     WithFinDeSuiviMixin,
+    WithFormErrorsAsMessagesMixin,
+    WithFreeLinksListInContextMixin,
+    WithMessageMixin,
 )
+from core.views import MediaDefiningMixin
+
+from ..forms import InvestigationCasHumainForm
+from ..formsets import InvestigationCasHumainsEtablissementFormSet
 from ..models import EvenementInvestigationCasHumain
+from .mixins import EvenementProduitValuesMixin
+
+
+class InvestigationCasHumainCreateView(
+    WithFormErrorsAsMessagesMixin,
+    WithAddUserContactsMixin,
+    EvenementProduitValuesMixin,
+    MediaDefiningMixin,
+    CreateView,
+):
+    template_name = "ssa/evenement_investigation_cas_humain.html"
+    form_class = InvestigationCasHumainForm
+    success_message = "L’évènement Investigation de cas humain a été créé avec succès."
+
+    @property
+    def etablissement_formset(self):
+        if not hasattr(self, "_etablissement_formset"):
+            self._etablissement_formset = InvestigationCasHumainsEtablissementFormSet(**super().get_form_kwargs())
+        return self._etablissement_formset
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["user"] = self.request.user
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs, etablissement_formset=self.etablissement_formset)
+
+    def get_media(self, **context_data) -> Media:
+        return super().get_media(**context_data) + context_data["etablissement_formset"].media
+
+    def form_valid(self, form):
+        self.object = form.save()
+        self.etablissement_formset.instance = self.object
+        self.etablissement_formset.save()
+        self.add_user_contacts(self.object)
+
+        messages.success(self.request, self.success_message)
+        return HttpResponseRedirect(self.object.get_absolute_url())
+
+    def formset_invalid(self):
+        self.object = None
+        messages.error(
+            self.request,
+            "Erreurs dans le(s) formulaire(s) Etablissement",
+        )
+        for i, form in enumerate(self.etablissement_formset):
+            if not form.is_valid():
+                for field, errors in form.errors.items():
+                    for error in errors:
+                        messages.error(
+                            self.request, f"Erreur dans le formulaire établissement #{i + 1} : '{field}': {error}"
+                        )
+
+        return self.render_to_response(self.get_context_data())
+
+    def post(self, request, *args, **kwargs):
+        if not self.etablissement_formset.is_valid():
+            return self.formset_invalid()
+
+        form = self.get_form()
+        if not form.is_valid():
+            return self.form_invalid(form)
+        return self.form_valid(form)
+
+
+class InvestigationCasHumainUpdateView(InvestigationCasHumainCreateView, UpdateView):
+    success_message = "La fiche d'investigation cas humain a été mise à jour avec succès."
+
+    @property
+    def object(self):
+        if not hasattr(self, "_object"):
+            self._object = self.get_object()
+        return self._object
+
+    @object.setter
+    def object(self, value):
+        self._object = value
+
+    def get_queryset(self):
+        return EvenementInvestigationCasHumain.objects.with_departement_prefetched()
 
 
 class InvestigationCasHumainDetailView(
