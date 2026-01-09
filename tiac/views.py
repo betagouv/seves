6efad68014c5d6efe5e5bc027e96e908343f459b
Anto59290 <antonin59290@hotmail.com>
2026-01-09 09:34:50 +0100

    Improve history view for evenement simple
    - Change order of entries
    - Add user for free links
    - Add new lines on the investigation tiac created after the
      transformation

--- tiac/views.py
+++ tiac/views.py
@@ -33,7 +33,7 @@ from core.mixins import (
     WithExportHeterogeneousQuerysetMixin,
     WithFormsetInvalidMixin,
 )
-from core.models import LienLibre
+from core.models import LienLibre, CustomRevisionMetaData
 from core.views import MediaDefiningMixin
 from ssa.constants import CategorieDanger, CategorieProduit
 from ssa.models.mixins import build_combined_options
@@ -275,6 +275,12 @@ class EvenementTransformView(UpdateView):
             setattr(self.investigation, field, getattr(self.object, field))
         self.investigation.follow_up = InvestigationFollowUp.INVESTIGATION_DD
         self.investigation.save()
+        revision = create_manual_version(
+            self.investigation,
+            f"L'événement a été créé depuis un enregistrement simple ({self.object.numero}) passé en investigation de TIAC",
+            user=self.request.user,
+        )
+        CustomRevisionMetaData.objects.create(revision=revision, extra_data={"field": "Passé en investigation TIAC"})
 
     def _copy_etablissements(self):
         for etablissement in self.object.etablissements.all():
@@ -288,28 +294,33 @@ class EvenementTransformView(UpdateView):
             if self.object == free_link.related_object_1:
                 free_link.pk = None
                 free_link.related_object_1 = self.investigation
+                free_link._user = self.request.user
                 free_link.save()
             else:
                 free_link.pk = None
                 free_link.related_object_2 = self.investigation
+                free_link._user = self.request.user
                 free_link.save()
 
-        LienLibre.objects.create(related_object_1=self.object, related_object_2=self.investigation)
+        free_link = LienLibre(related_object_1=self.object, related_object_2=self.investigation)
+        free_link._user = self.request.user
+        free_link.save()
 
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
-        for contact in self.object.get_contacts_structures_not_in_fin_suivi():
-            self.object.add_fin_suivi(structure=contact.structure, made_by=self.request.user)
         self._create_investigation_tiac()
-        self.object.cloturer()
-        self._copy_etablissements()
-        self._copy_and_add_free_links()
-        notify_transformation(self.object, self.investigation)
-        create_manual_version(
+        revision = create_manual_version(
             self.object,
             f"L'événement a été passé en investigation de TIAC ({self.investigation.numero})",
             user=self.request.user,
         )
+        CustomRevisionMetaData.objects.create(revision=revision, extra_data={"field": "Passé en investigation TIAC"})
+        for contact in self.object.get_contacts_structures_not_in_fin_suivi():
+            self.object.add_fin_suivi(structure=contact.structure, made_by=self.request.user)
+        self.object.cloturer()
+        self._copy_etablissements()
+        self._copy_and_add_free_links()
+        notify_transformation(self.object, self.investigation)
         messages.success(self.request, "L'événement a bien été passé en investigation de TIAC.")
         return HttpResponseRedirect(reverse("tiac:investigation-tiac-edition", kwargs={"pk": self.investigation.pk}))
 
