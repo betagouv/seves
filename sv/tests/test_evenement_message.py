108c21e07f37affd24aca70a9559a5f19ec657f7
Anto59290 <antonin59290@hotmail.com>
2026-02-05 16:28:57 +0100

    Fix all ChoicesJs list search
    By default ChoiceJs is configured to search in both the value and the
    label of the options. This is kind of broken in our situation, let's say
    we have:
    
    - A contact for structure "DDPP 11", with whatever id/pk.
    - A contact for structure "My structure", with id 11
    
    When searching for "11" in the field, we will both contacts in the
    results. It is very unlikely that a user is search by id instead of
    label.
    
    We restrict the search on the label by default on all ChoicesJs
    components.
    
    https://github.com/Choices-js/Choices?tab=readme-ov-file#searchfields

--- sv/tests/test_evenement_message.py
+++ sv/tests/test_evenement_message.py
@@ -1142,3 +1142,25 @@ def test_contact_shorcut_excludes_agent_and_structures_in_fin_suivi(live_server,
 
 def test_can_search_in_message_list(live_server, page: Page):
     generic_test_can_search_in_message_list(live_server, page, EvenementFactory())
+
+
+def test_choices_js_only_search_in_label(live_server, page: Page, choice_js_get_all_values):
+    evenement = EvenementFactory()
+    page.goto(f"{live_server.url}{evenement.get_absolute_url()}")
+
+    ContactStructureFactory(structure__libelle="Structure 11", structure__force_can_be_contacted=True)
+    ContactStructureFactory(structure__libelle="Ma structure", id=11, structure__force_can_be_contacted=True)
+    page.get_by_test_id("element-actions").click()
+    page.get_by_role("link", name="Message", exact=True).click()
+    page.wait_for_url("**core/message**")
+
+    results = choice_js_get_all_values(page, 'label[for="id_recipients"] ~ div.choices')
+    assert "Ma structure" in results
+    assert "Structure 11" in results
+
+    page.locator('label[for="id_recipients"] ~ div.choices').click()
+    page.wait_for_selector("input:focus", state="visible", timeout=2_000)
+    page.locator("*:focus").fill("11")
+    results = choice_js_get_all_values(page, 'label[for="id_recipients"] ~ div.choices')
+    assert "Ma structure" not in results
+    assert "Structure 11" in results
