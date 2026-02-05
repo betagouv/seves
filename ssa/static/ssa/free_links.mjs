1ae12ad56e895b36c279aa4b22774f1e793b1623
Anto59290 <antonin59290@hotmail.com>
2026-02-05 12:42:15 +0100

    Allow to search free links via API
    Instead of loading all the events on page load (20K+ events) we make
    use an API to perform a search.
    
    - Avoid looking over 20K+ element in the template rendering of the
      options
    - Avoid ChoiceJs parsing 20K+ option tags on page load
    
    We think that most of the users won't add a free link and will perform
    other change on this page so we don't need to have all the choices on
    page load.

--- ssa/static/ssa/free_links.mjs
+++ ssa/static/ssa/free_links.mjs
@@ -1,3 +1,70 @@
-import {setUpFreeLinks} from "Freelinks";
+import {applicationReady} from "Application";
+import {Controller} from "Stimulus";
+import choicesDefaults from "choicesDefaults"
 
-setUpFreeLinks(document.getElementById("id_free_link"), document.getElementById('free-links-id'))
+class FreeLinksViaApiController  extends Controller {
+    static targets = [
+        "select",
+        "input"
+    ]
+
+    debounce(func, wait) {
+        let timeout;
+        return function (...args) {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => func.apply(this, args), wait);
+        };
+    }
+
+    fetchFreeLink(query) {
+        return fetch(`/ssa/api/freelinks/recherche/?q=${query}`)
+            .then(async response => {
+                const data = await response.json();
+                return data.results;
+            }, error => {
+                console.error('Erreur lors de la récupération des données:', error);
+                return [];
+            })
+    }
+
+    syncChoicesToSelect() {
+        this.inputTarget.querySelectorAll('option').forEach(o => o.selected = false);
+
+        this.freeLinksChoices.getValue(true).forEach(value => {
+            let option = this.inputTarget.querySelector(`option[value="${value}"]`);
+            if (!option) {
+                option = new Option(value, value, true, true);
+                this.inputTarget.add(option);
+            }
+            option.selected = true;
+        });
+    }
+
+
+    connect() {
+        this.freeLinksChoices = new Choices(this.selectTarget, {
+            ...choicesDefaults,
+            searchResultLimit: 500,
+            removeItemButton: true,
+            noChoicesText: 'Aucune fiche à sélectionner',
+            searchFields: ['label'],
+        });
+        this.freeLinksChoices.input.element.addEventListener('input', this.debounce(() =>{
+            const query = this.freeLinksChoices.input.element.value
+            if (query.length >= 5) {
+                this.fetchFreeLink(query).then(results => {
+                    this.freeLinksChoices.setChoices(results, 'value', 'label', false)
+                })
+            }
+        }, 300))
+
+        this.freeLinksChoices.passedElement.element.addEventListener('change', () => {
+            this.syncChoicesToSelect();
+        });
+        this.syncChoicesToSelect();
+    }
+}
+
+applicationReady.then(app => {
+    app.register("free-link-via-api", FreeLinksViaApiController)
+})
