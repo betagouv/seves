f8431c27930db8180eabac5104e707ccb3257c49
Anto59290 <antonin59290@hotmail.com>
2026-02-05 16:28:57 +0100

    Fix all ChoicesJs list search
    By default ChoiceJs is configured to search in both the value and the
    label of the options. This is kind of broken in our situation, let's say
    we have:
    
    - A contact for structure "DDPP 11", with whatever id/pk.
    - A contact for structure "My structure", with id 11
    
    When searching for "11" in the field, we will both contacts in the
    results. It is very unlikely that a user is search by id instead of
    label.
    
    We restrict the search on the label by default on all ChoicesJs
    components.
    
    https://github.com/Choices-js/Choices?tab=readme-ov-file#searchfields

--- core/static/core/choices.mjs
+++ core/static/core/choices.mjs
@@ -2,6 +2,7 @@ export const choicesDefaults = {
     noResultsText: "Aucun résultat trouvé",
     noChoicesText: "Aucun résultat trouvé",
     searchResultLimit: 10,
+    searchFields: ["label"],
     classNames: {containerInner: "fr-select"},
     itemSelectText: "",
     shouldSort: false,
