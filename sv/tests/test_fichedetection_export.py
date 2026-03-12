eba18ba5ae2ffa471536973f8ba32272e5f4f667
Anto59290 <antonin59290@hotmail.com>
2026-03-12 15:54:46 +0100

    Make sure the CSV export in SV respects filters
    Make sure we take all the filters (year, state, contacts, etc.) into
    account when creating the CSV export instead of exporting more or less
    the whole database.
    We need to transform the queryset a bit because the list view is listing
    Evenements objects and the CSV is listing FicheDetection objects.

--- sv/tests/test_fichedetection_export.py
+++ sv/tests/test_fichedetection_export.py
@@ -1,5 +1,6 @@
+import csv
+
 from django.urls import reverse
-from playwright.sync_api import expect
 
 from sv.factories import FicheDetectionFactory
 
@@ -8,11 +9,30 @@ def test_can_download_export_fiche_detection(live_server, page):
     FicheDetectionFactory()
     page.goto(f"{live_server.url}{reverse('sv:evenement-liste')}")
 
-    page.get_by_test_id("extract-open").click()
-    expect(page.get_by_test_id("extract-submit")).to_be_visible()
-
     with page.expect_download() as download_info:
-        page.get_by_test_id("extract-submit").click()
+        page.get_by_test_id("extract").click()
 
     download = download_info.value
     assert download.suggested_filename == "export_fiche_detection.csv"
+
+
+def test_export_is_filtered(live_server, page):
+    FicheDetectionFactory(evenement__numero_annee=2025)
+    FicheDetectionFactory(evenement__numero_annee=2026)
+    page.goto(f"{live_server.url}{reverse('sv:evenement-liste')}")
+
+    page.get_by_label("Année").fill("2025")
+    page.get_by_role("button", name="Rechercher").click()
+
+    with page.expect_download() as download_info:
+        page.get_by_test_id("extract").click()
+
+    download = download_info.value
+    assert download.suggested_filename == "export_fiche_detection.csv"
+
+    path = download.path()
+    with open(path, "r", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        lines = list(reader)
+
+    assert len(lines) == 2
