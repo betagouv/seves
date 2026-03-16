bc56f6f00e43210fd50396660376edb13bc85134
Anto59290 <antonin59290@hotmail.com>
2026-03-16 08:36:06 +0100

    Improves Django admin
    Make sure the django admin can be used for some models.

--- tiac/admin.py
+++ tiac/admin.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 
 from .models import AlimentSuspect, AnalyseAlimentaire, Etablissement, EvenementSimple, InvestigationTiac, RepasSuspect
 
-admin.site.register(EvenementSimple)
+
+class InvestigationTiacAdmin(admin.ModelAdmin):
+    raw_id_fields = ("contacts",)
+
+
+class EvenementSimpleAdmin(admin.ModelAdmin):
+    raw_id_fields = ("contacts",)
+
+
+admin.site.register(EvenementSimple, EvenementSimpleAdmin)
 admin.site.register(Etablissement)
-admin.site.register(InvestigationTiac)
+admin.site.register(InvestigationTiac, InvestigationTiacAdmin)
 admin.site.register(RepasSuspect)
 admin.site.register(AlimentSuspect)
 admin.site.register(AnalyseAlimentaire)
