d8390a812c2366d6dd985d9470a16c71188bdbbf
Anto59290 <antonin59290@hotmail.com>
2026-05-07 08:39:29 +0200

    Improve page to give admin rights
    - Allow to remove the right to a specific app or add only the right to a
      specific app without having to remove all the rights and add them
    back.
    - Allow to filter by Structure in the table to ease the use of the
      table.
    - Improve performances (reduce number of SQL queries)
    - Add tests
    - Change SSA to Alim

--- account/tests/test_handle_admin_permission_view.py
+++ account/tests/test_handle_admin_permission_view.py
@@ -43,16 +43,16 @@ def test_can_add_admin_permissions(
 
     page.goto(f"{live_server.url}/{reverse('handle-admins')}")
     expect(page.locator("table").get_by_text(agent_already_admin.agent_with_structure, exact=True)).to_be_visible()
-    assert agent_already_admin.agent_with_structure not in choice_js_get_all_values(page, ".choices")
+    assert agent_already_admin.agent_with_structure in choice_js_get_all_values(page, ".choices")
 
     choice_js_fill(page, ".choices", agent_will_be_admin.agent_with_structure, agent_will_be_admin.agent_with_structure)
-    page.get_by_label("SSA").check(force=True)
+    page.locator(".white-container").get_by_text("Alim", exact=True).check()
     page.get_by_role("button", name="Accorder le rôle administrateur").click()
     page.get_by_role("button", name="Confirmer le rôle d’administrateur").click()
     expect(page.get_by_text("Le rôle administrateur a été accordé")).to_be_visible()
 
     expect(page.locator("table").get_by_text(agent_will_be_admin.agent_with_structure, exact=True)).to_be_visible()
-    assert agent_will_be_admin.agent_with_structure not in choice_js_get_all_values(page, ".choices")
+    assert agent_will_be_admin.agent_with_structure in choice_js_get_all_values(page, ".choices")
 
     agent_will_be_admin.refresh_from_db()
     assert agent_will_be_admin.user.is_active is True
@@ -78,3 +78,81 @@ def test_can_remove_admin_permissions(live_server, page, mocked_authentification
     agent_already_admin.refresh_from_db()
     assert agent_already_admin.user.is_active is True
     assert set(agent_already_admin.user.groups.values_list("name", flat=True)) == {SSA_GROUP}
+
+
+@pytest.mark.django_db
+def test_performances_admin_page_scales(client, mocked_authentification_user, django_assert_num_queries):
+    structure, _ = Structure.objects.get_or_create(niveau1=AC_STRUCTURE, niveau2=MUS_STRUCTURE)
+    mocked_authentification_user.agent.structure = structure
+    AgentFactory(with_active_user__with_groups=[settings.CAN_GIVE_ACCESS_GROUP, settings.SSA_GROUP])
+
+    with django_assert_num_queries(10):
+        response = client.get(reverse("handle-admins"))
+        assert response.status_code == 200
+
+    AgentFactory(with_active_user__with_groups=[settings.CAN_GIVE_ACCESS_GROUP, settings.SSA_GROUP])
+    AgentFactory(with_active_user__with_groups=[settings.CAN_GIVE_ACCESS_GROUP, settings.SSA_GROUP])
+    AgentFactory(with_active_user__with_groups=[settings.CAN_GIVE_ACCESS_GROUP, settings.SSA_GROUP])
+
+    with django_assert_num_queries(10):
+        response = client.get(reverse("handle-admins"))
+        assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_can_add_admin_permissions_to_user_with_existing_permissions(
+    live_server, page, mocked_authentification_user, choice_js_get_all_values, choice_js_fill
+):
+    sv_group, _ = Group.objects.get_or_create(name=settings.SV_GROUP)
+    ssa_group, _ = Group.objects.get_or_create(name=settings.SSA_GROUP)
+    access_admin_group, _ = Group.objects.get_or_create(name=CAN_GIVE_ACCESS_GROUP)
+    structure, _ = Structure.objects.get_or_create(niveau1=AC_STRUCTURE, niveau2=MUS_STRUCTURE)
+    mocked_authentification_user.agent.structure = structure
+
+    agent_already_admin = AgentFactory(
+        with_active_user__with_groups=[settings.CAN_GIVE_ACCESS_GROUP, settings.SV_GROUP]
+    )
+    ContactAgentFactory(agent=agent_already_admin)
+
+    page.goto(f"{live_server.url}/{reverse('handle-admins')}")
+    expect(page.locator("table").get_by_text(agent_already_admin.agent_with_structure, exact=True)).to_be_visible()
+
+    choice_js_fill(page, ".choices", agent_already_admin.agent_with_structure, agent_already_admin.agent_with_structure)
+    page.locator(".white-container").get_by_text("Alim", exact=True).check()
+    page.locator(".white-container").get_by_text("SV", exact=True).uncheck()
+    page.get_by_role("button", name="Accorder le rôle administrateur").click()
+    page.get_by_role("button", name="Confirmer le rôle d’administrateur").click()
+    expect(page.get_by_text("Le rôle administrateur a été accordé")).to_be_visible()
+
+    expect(page.locator("table").get_by_text(agent_already_admin.agent_with_structure, exact=True)).to_be_visible()
+    assert agent_already_admin.agent_with_structure in choice_js_get_all_values(page, ".choices")
+
+    agent_already_admin.refresh_from_db()
+    assert agent_already_admin.user.is_active is True
+    assert set(agent_already_admin.user.groups.values_list("name", flat=True)) == {CAN_GIVE_ACCESS_GROUP, SSA_GROUP}
+
+
+@pytest.mark.django_db
+def test_can_filter_table_on_structure(live_server, page, mocked_authentification_user):
+    access_admin_group, _ = Group.objects.get_or_create(name=CAN_GIVE_ACCESS_GROUP)
+    structure, _ = Structure.objects.get_or_create(niveau1=AC_STRUCTURE, niveau2=MUS_STRUCTURE)
+    mocked_authentification_user.agent.structure = structure
+
+    agent_already_admin_1 = AgentFactory(
+        with_active_user__with_groups=[settings.CAN_GIVE_ACCESS_GROUP], structure__libelle="DDPP1"
+    )
+    agent_already_admin_2 = AgentFactory(
+        with_active_user__with_groups=[settings.CAN_GIVE_ACCESS_GROUP], structure__libelle="DDPP2"
+    )
+
+    page.goto(f"{live_server.url}/{reverse('handle-admins')}")
+    expect(page.locator("table").get_by_text(agent_already_admin_1.agent_with_structure, exact=True)).to_be_visible()
+    expect(page.locator("table").get_by_text(agent_already_admin_2.agent_with_structure, exact=True)).to_be_visible()
+
+    page.get_by_label("Structure").select_option("DDPP1")
+    page.get_by_role("button", name="Rechercher").click()
+
+    expect(page.locator("table").get_by_text(agent_already_admin_1.agent_with_structure, exact=True)).to_be_visible()
+    expect(
+        page.locator("table").get_by_text(agent_already_admin_2.agent_with_structure, exact=True)
+    ).not_to_be_visible()
