7a1c6d6becb4195dbe536f6465371397c41be083
Anto59290 <antonin59290@hotmail.com>
2026-06-19 10:49:19 +0200

    Fix issue with values when repas modal is closed
    Make sure that when the modal is closed (except when using the save
    button) the values are not kept.
    I had to remove a ligne from the HTML as this line is duplicated in the
    same file, leading to a function being called twice and messing with the
    process.

--- tiac/static/tiac/repas.mjs
+++ tiac/static/tiac/repas.mjs
@@ -20,6 +20,11 @@ class RepasFormController extends BaseFormInModal {
     }
 
     onCloseForm() {
+        if (!this.keepChangesValue) {
+            this.restoreForm()
+        }
+        this.keepChangesValue = false
+
         // this.shouldImmediatelyShowValue indicates that the card has not be rendered yet.
         // In this case, the form is not considered valid and it should be deleted on close
         if (this.shouldImmediatelyShowValue) this.forceDelete()
