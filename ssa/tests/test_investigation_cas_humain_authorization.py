ba794f73d9a033d126d9d583322451109449171b
Anto59290 <antonin59290@hotmail.com>
2026-07-22 16:20:25 +0200

    Make sure we can only cloturer/reopen with group
    Make sure we check for the group before we allow to do this kind of
    actions.

--- ssa/tests/test_investigation_cas_humain_authorization.py
+++ ssa/tests/test_investigation_cas_humain_authorization.py
@@ -1,7 +1,9 @@
 from core.tests.generic_tests.authorization import (
     generic_test_cant_add_agent_without_domain_group,
     generic_test_cant_add_structure_without_domain_group,
+    generic_test_cant_cloturer_without_domain_group,
     generic_test_cant_delete_document_without_domain_group,
+    generic_test_cant_open_without_domain_group,
     generic_test_cant_publish_without_domain_group,
     generic_test_cant_soft_delete_without_domain_group,
     generic_test_cant_upload_document_without_domain_group,
@@ -40,3 +42,15 @@ def test_cant_publish_without_domain_group(client, mocked_authentification_user)
 def test_cant_soft_delete_without_domain_group(client, mocked_authentification_user):
     evenement = InvestigationCasHumainFactory(etat=EvenementInvestigationCasHumain.Etat.EN_COURS)
     generic_test_cant_soft_delete_without_domain_group(client, mocked_authentification_user, evenement)
+
+
+def test_cant_cloturer_without_group(client, mocked_authentification_user, mus_contact):
+    evenement = InvestigationCasHumainFactory(etat=EvenementInvestigationCasHumain.Etat.EN_COURS)
+    generic_test_cant_cloturer_without_domain_group(
+        client, mocked_authentification_user, evenement, mus_contact.structure
+    )
+
+
+def test_cant_ouvrir_without_group(client, mocked_authentification_user, mus_contact):
+    evenement = InvestigationCasHumainFactory(etat=EvenementInvestigationCasHumain.Etat.CLOTURE)
+    generic_test_cant_open_without_domain_group(client, mocked_authentification_user, evenement, mus_contact.structure)
