betagouv/seves

commit 673bd93e4c83ac981f28a12bf1280c678b8c0006

fix(treeselect): fix treeselect search focus making treeselect tests flaky

core/static/core/form/widgets/treeselect_dsfr.mjs
@@ -56,11 +56,7 @@ dsfrInitialized.then(dsfr => {
                     it.dispatchEvent(new Event("input"))
                 }
             })
-            this.addDescent(this.constructor.FOCUS, () => {
-                for (const it of this.node.querySelectorAll('.fr-input[type="search"]')) {
-                    setTimeout(() => requestAnimationFrame(() => it.focus()), 50)
-                }
-            })
+            this.addDescent(this.constructor.FOCUS, () => this.node.querySelector('.fr-input[type="search"]')?.focus())
         }
 
         onChange({target: {value}}) {

commit 9e91de20f7e10f1c76e1952eac70ce211344a541

fix: revert 673bd93 mistakenly pushed to prod

core/static/core/form/widgets/treeselect_dsfr.mjs
@@ -56,7 +56,11 @@ dsfrInitialized.then(dsfr => {
                     it.dispatchEvent(new Event("input"))
                 }
             })
-            this.addDescent(this.constructor.FOCUS, () => this.node.querySelector('.fr-input[type="search"]')?.focus())
+            this.addDescent(this.constructor.FOCUS, () => {
+                for (const it of this.node.querySelectorAll('.fr-input[type="search"]')) {
+                    setTimeout(() => requestAnimationFrame(() => it.focus()), 50)
+                }
+            })
         }
 
         onChange({target: {value}}) {